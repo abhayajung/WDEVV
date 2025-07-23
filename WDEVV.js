84fb0469e44aaf12229659125454517d875cdf79
Abhaya Jung Khadka <jungabhayakhadka333@gmail.com>
2025-07-23 23:17:14 +0545

    new changes

--- WDEVV.js
+++ WDEVV.js
@@ -1,16 +1,3 @@
-// Math object
-console.log(Math);
-console.log(Math.PI);
-console.log(Math.E);
+const para = document.querySelectorAll("p");
 
-const area = 7.3;
-console.log(Math.round(area));
-console.log(Math.floor(area));
-console.log(Math.ceil(area));
-console.log(Math.trunc(area));
-
-// random numbers
-
-const random = Math.random();
-console.log(random);
-console.log(Math.ceil(random));
+console.log(para[2]);
