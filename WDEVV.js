ab388a269701fecf450eefa2abe61700fadc33be
Abhaya Jung Khadka <jungabhayakhadka333@gmail.com>
2025-07-26 23:17:49 +0545

    new chnges

--- WDEVV.js
+++ WDEVV.js
@@ -1,1 +1,19 @@
+function SOLVE() {
+  var a = parseInt(document.getElementById("tb1").value);
+  var b = parseInt(document.getElementById("tb2").value);
+  var c = parseInt(document.getElementById("tb3").value);
 
+  switch (c) {
+    case 1:
+      alert("sum is " + (a + b));
+      break;
+    case 2:
+      alert("prodcut is " + a * b);
+      break;
+    case 3:
+      alert("average is " + (a + b) / 2);
+      break;
+    default:
+      alert("Wrong choice");
+  }
+}
